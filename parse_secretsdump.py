e3f371b9f778646e8753493eb5e232617d6213c4
demforce <57765592+demforce@users.noreply.github.com>
2022-09-28 10:05:23 +0200

    Add files via upload

---
+++ parse_secretsdump.py
@@ -0,0 +1,45 @@
+# Taken two files, matches user with cleartext pwd
+import sys, os
+
+creds = []
+
+def usage():
+    print ("[+] Usage: " + sys.argv[0] + " secretsdump crackedhashes outfile")
+    print("secretsdump must be: Path to file containing user:id:domainHash:userHash\n""crackedhashes must be: Path to file containing hash:cleartextPwd\n""outfile: path to output file")
+    print("Examples:\nsecretsdump: Administrator:12131:aad3b435b51404eeaad3b435b51404ee:8846F7EAEE8FB117AD06BDD830B7586C::: ")
+    print("crackedhashes: 8846F7EAEE8FB117AD06BDD830B7586C:password ")
+    
+def readfile():                                                             # importo i 3 file 
+    file_hash = sys.argv[1]
+    file_clear = sys.argv[2]
+    outfile = sys.argv[3]
+
+    f1 = open(file_hash,"r")
+    lines1 = f1.readlines()                                                  # user:id:domainhash:hash..
+    f1.close()
+    
+    f2 = open(file_clear,"r")
+    lines2 = f2.readlines()                                                  # hash:cleartext
+    f2.close()
+    
+    return lines1, lines2, outfile
+
+def main():
+        
+    if os.path.exists(readfile()[2]):
+        os.remove(readfile()[2])
+
+    f3 = open(readfile()[2], "a")                                           # apro outfile per scrittura
+    
+    for y in readfile()[1]:                                                 # leggo le righe di file_hash
+        for i in readfile()[0]:                                             # leggo le righe di file_clear    
+            if y.split(":")[0].lower() == i.split(":")[3].lower():                          # if hash = hash allora sostituisco con cleartext
+                #print (i.split(":")[0] + ":" + y.split(":")[1], end ="")
+                f3.write(i.split(":")[0] + ":" + y.split(":")[1])           # scrivo su file l'output
+
+    f3.close()            
+
+if len(sys.argv) > 3:
+    main()
+else:
+    usage()
